06b80343ee580e945202c526cc92f51974969cac
marvspreng <marv.sprengel@gmail.com>
2022-12-19 16:53:56 +0100

    moved some folders, cleaned some folders

--- Repository/_AutoInstaller/collection/PointRender/init.py
+++
@@ -1,7 +0,0 @@
-#Higx Tools
-nuke.pluginAddPath('./Icons')
-nuke.pluginAddPath('./Tools')
-nuke.pluginAddPath('./Tools/General')
-nuke.pluginAddPath('./Tools/Generators')
-nuke.pluginAddPath('./Tools/Modifiers')
-nuke.pluginAddPath('./Tools/Shaders')
